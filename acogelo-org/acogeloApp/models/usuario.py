f4d07325bb0b2c0f1a8d6e527c617909b3aefb56
bryan-gomez-hincapie <bryan.gomez_hincapie@uao.edu.co>
2021-10-08 17:40:19 -0500

    Creacion del modelo para la tabla usuarios

--- acogelo-org/acogeloApp/models/usuario.py
+++ acogelo-org/acogeloApp/models/usuario.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .mascota import Mascota
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.contrib.auth.hashers import make_password
 
@@ -30,8 +31,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     id = models.BigAutoField(primary_key=True)
     username = models.CharField('Username', max_length = 15, unique=True)
     password = models.CharField('Password', max_length = 256)
-    name = models.CharField('Name', max_length = 30)
+    nombres: models.CharField(max_length = 100, unique=False)
+    apellidos: models.CharField(max_length = 100, unique=False)
     email = models.EmailField('Email', max_length = 100)
+    rol: models.CharField(max_length = 1, unique=False)
+    celular: models.BigIntegerField()
+    horario_contacto: models.CharField(max_length = 50, unique=False)
+    dpto_residencia: models.CharField(max_length = 50, unique=False)
+    ciudad_residencia: models.CharField(max_length = 50, unique=False)
+    aceptacion_termycond: models.BooleanField(default=False, unique=False)
+    id_masc_usuario: models.ForeignKey(Mascota, related_name='mascotas', on_delete=models.CASCADE)
     
     def save(self, **kwargs):
         some_salt = 'mMUj0DrIK6vgtdIYepkIxN'
