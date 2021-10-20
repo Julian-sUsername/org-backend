641194821187d4de8143e40a1180b5915d0f373d
astromartez <astrid.martinez.com@gmail.com>
2021-10-20 00:09:46 -0500

    Comentado base de datos

--- acogelo-be/acogeloProject/settings.py
+++ acogelo-be/acogeloProject/settings.py
@@ -116,6 +116,7 @@ WSGI_APPLICATION = 'acogeloProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -133,7 +134,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 
 
 # Password validation
