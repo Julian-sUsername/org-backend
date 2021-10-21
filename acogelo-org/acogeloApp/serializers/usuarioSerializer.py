d73f17b33e170059bffc4da4b6e77457e13b6661
astromartez <astrid.martinez.com@gmail.com>
2021-10-20 23:28:36 -0500

    Corrección serializer

--- acogelo-org/acogeloApp/serializers/usuarioSerializer.py
+++ acogelo-org/acogeloApp/serializers/usuarioSerializer.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from django.db import models
-#from acogeloApp.models.usuario import User
+from acogeloApp.models.usuario import User
 from acogeloApp.models.mascota import Mascota
 from acogeloApp.serializers.mascotaSerializer import MascotaSerializer
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'nombres', 'apellidos','rol', 'celular', 'horario_contacto', 'dpto_residencia', 'ciudad_residencia', 'aceptacion_termycond', 'mascota']
+        fields = ['username', 'email', 'nombres', 'apellidos','rol', 'celular', 'horario_contacto', 'dpto_residencia', 'ciudad_residencia', 'aceptacion_termycond']
