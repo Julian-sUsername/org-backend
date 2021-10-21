9632d8fabc6c39d65ed78a9c0143328786248291
astromartez <astrid.martinez.com@gmail.com>
2021-10-20 20:35:46 -0500

    Ajustes al nuevo modelo relacion

--- acogelo-org/acogeloApp/models/relacion.py
+++ acogelo-org/acogeloApp/models/relacion.py
@@ -2,7 +2,7 @@ from django.db import models
 from .mascota import Mascota
 from .usuario import User
 
-class Mascota(models.Model):
+class Relacion(models.Model):
     id_masc = models.ForeignKey(Mascota, related_name='mascota', on_delete=models.CASCADE)    
     id_user = models.ForeignKey(User, related_name='usuario', on_delete=models.CASCADE)
     tipo_relacion = models.CharField(max_length = 20, unique=False)
